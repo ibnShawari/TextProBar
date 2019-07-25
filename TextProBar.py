1de3fdd0c27b062e6899eac24add937078d67683
ibnShawari <1263755669@qq.com>
2019-07-25 22:27:51 +0800

    单行刷新

--- TextProBar.py
+++ TextProBar.py
@@ -1,12 +1,14 @@
 #TextProBar
 # -*- coding:utf-8 -*-
 import time
-scale = 10
+scale = 50
 print("------执行开始------")
+start = time.perf_counter()
 for i in range(scale+1):
     a = '*' * i
     b = '.' * (scale - i)
     c = (i/float(scale))*100
-    print ("{:^3.0f}%[{}->{}]".format(c,a,b))
+    dur = time.perf_counter() - start
+    print ("\r{:^3.0f}%[{}->{}]{:.2f}s".format(c,a,b,dur), end = "")
     time.sleep(0.1)
-print ("------执行结束------")
+print ("\n------执行结束------")
